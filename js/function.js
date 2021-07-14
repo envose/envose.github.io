0d13eef2157961ae95c90e0d7176578341e68de5
envose <envose.au@gmail.com>
2021-07-14 16:52:15 +0800

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -27,6 +27,35 @@ function submitTeam() {
   }
 }
 
+function initModalAlert(classification, title, body, btn, callback, btn_close) {
+  var m_title = document.getElementById('alertModalLabel');
+  var m_body = document.getElementById('alertModalBody');
+  var m_footer = document.getElementById('alertModalFooter');
+
+  m_title.innerHTML = '';
+  m_title.innerHTML = title;
+
+  m_body.innerHTML = '';
+  m_body.innerHTML = body;
+
+  m_footer.innerHTML = '';
+  var btn_c = document.createElement('button');
+  btn_c.classList.add(...['btn', 'btn-secondary']);
+  btn_c.setAttribute('data-dismiss', 'modal');
+  btn_c.innerHTML = btn_close;
+  m_footer.appendChild(btn_c);
+
+  var btn_ = document.createElement('button');
+  btn_.classList.add(...['btn', classification]);
+  btn_.innerHTML = btn;
+  btn_.onclick = callback;
+  m_footer.appendChild(btn_);
+
+
+          // <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          // <button type="button" class="btn btn-danger">Save changes</button>
+
+}
 
 
 
--- js/view.js
+++ js/view.js
@@ -91,13 +91,18 @@ function createAddEntry(div) {
 
 function createTeamView() {
   initViews();
+  numOfMemEntry = 0;
 
   // header
   setHeaderTitle('h2', getSysTranslate('form_team'));
 
-  // footer
-  var div = createCustomElement('div', 'view_content_center');
+  // content
+  var div2 = document.createElement('div');
+  div2.classList.add('col-12');
 
+  // var second = document.createElement('div');
+  // second.classList.add('col-12');
+  var b_div = createCustomElement('div', 'view_content_center');
   var entry = createCustomElement('div', 'view_entry');
   var addbtn = createCustomElement('button', 'btn_free_info');
   addbtn.id = 'addbtn';
@@ -105,19 +110,74 @@ function createTeamView() {
   addbtn.innerHTML = getSysTranslate('add');
   addbtn.onclick = function () { createAddEntry(c_div); };
   entry.appendChild(addbtn);
+  b_div.appendChild(entry);
 
   var btn = createCustomElement('button', 'btn_free_primary');
   btn.innerHTML = getSysTranslate('confirm');
   btn.onclick = function() { submitTeam(); };
   entry.appendChild(btn);
-  div.appendChild(entry);
+  b_div.appendChild(entry);
+
+  div2.appendChild(b_div);
+
+  var div1 = document.createElement('div');
+  div1.classList.add('col-12');
+  var c_div = createCustomElement('div', 'view_content_center');
+  // c_div.classList.add('col-12');
+  createAddEntry(c_div);
+  div1.appendChild(c_div);
+
+  content.appendChild(div1);
+  content.appendChild(div2);
+  // content.appendChild(c_div);
+
+  // footer
+  var div3 = createCustomElement('div', 'view_content_center');
+
+  var btn_exit = createCustomElement('button', 'btn_free_danger');
+  btn_exit.classList.add('mt-5');
+  btn_exit.innerHTML = getSysTranslate('exit');
+  btn_exit.setAttribute('data-toggle', 'modal');
+  btn_exit.setAttribute('data-target', '#alertModal');
+  var callback = function () {
+    createLoginView();
+    $('#alertModal').modal('hide');
+  }
+  btn_exit.onclick = function() { initModalAlert('btn-danger', getSysTranslate('exit'), getSysTranslate('alert_exit'), getSysTranslate('continue'), callback, getSysTranslate('back')); };
+  div3.appendChild(btn_exit);
+
+  // div3.appendChild(div4);
+  footer.appendChild(div3);
+}
+
+function entryView() {
+  initViews();
+
+  // footer
+  var div = createCustomElement('div', 'view_content_center');
+  div.classList.add(...['mt-5', 'pt-5']);
+  var btn_back = createCustomElement('button', 'btn_free_light');
+  btn_back.innerHTML = getSysTranslate('back');
+  btn_back.classList.add(...['mt-5', 'pt-5']);
+  btn_back.onclick = function() { dashboard(); };
+  div.appendChild(btn_back);
   footer.appendChild(div);
 
-  // content
-  var c_div = createCustomElement('div', 'view_content_center');
-  createAddEntry(c_div);
+}
 
-  content.appendChild(c_div);
+function recordView() {
+  initViews();
+
+  // footer
+  var div = createCustomElement('div', 'view_content_center');
+  div.classList.add(...['mt-5', 'pt-5']);
+  var btn_back = createCustomElement('button', 'btn_free_light');
+  btn_back.innerHTML = getSysTranslate('back');
+  btn_back.classList.add(...['mt-5', 'pt-5']);
+  btn_back.onclick = function() { dashboard(); };
+  div.appendChild(btn_back);
+  footer.appendChild(div);
+
 }
 
 function dashboard() {
@@ -134,10 +194,12 @@ function dashboard() {
 
   var btn_entry = createCustomElement('btn', 'btn_std_primary');
   btn_entry.innerHTML = getSysTranslate('entry');
+  btn_entry.onclick = function() { entryView(); };
   div2.appendChild(btn_entry);
 
   var btn_record = createCustomElement('btn', 'btn_std_info');
   btn_record.innerHTML = getSysTranslate('record');
+  btn_record.onclick = function() { recordView(); };
   div2.appendChild(btn_record);
 
   div.appendChild(div2);
@@ -145,14 +207,20 @@ function dashboard() {
 
   // footer
   var div3 = createCustomElement('div', 'view_content_center');
-  var div4 = createCustomElement('div', 'view_col5');
+  // var div4 = createCustomElement('div', 'view_col5');
 
-  var btn_complete = createCustomElement('button', 'btn_std_danger');
-  btn_complete.classList.add('mt-5');
-  btn_complete.innerHTML = getSysTranslate('complete');
-  //btn.onclick = function() { login(); };
-  div4.appendChild(btn_complete);
+  var btn_dismiss = createCustomElement('button', 'btn_free_danger');
+  btn_dismiss.classList.add('mt-5');
+  btn_dismiss.innerHTML = getSysTranslate('dismiss');
+  btn_dismiss.setAttribute('data-toggle', 'modal');
+  btn_dismiss.setAttribute('data-target', '#alertModal');
+  var callback = function () {
+    createTeamView();
+    $('#alertModal').modal('hide');
+  }
+  btn_dismiss.onclick = function() { initModalAlert('btn-danger', getSysTranslate('dismiss'), getSysTranslate('alert_dismiss'), getSysTranslate('continue'), callback, getSysTranslate('back')); };
+  div3.appendChild(btn_dismiss);
 
-  div3.appendChild(div4);
+  // div3.appendChild(div4);
   footer.appendChild(div3);
 }
