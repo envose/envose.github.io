04dd931cc52bd19adfc9e2ea30c0a3b1497a518c
envose <envose.au@gmail.com>
2025-03-11 15:29:01 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -196,6 +196,7 @@ function genStampTable(res) {
     }
   }
   html+='</ul></div>';
+  return html;
   /*
   <li class="list-group-item d-flex justify-content-between align-items-center">
     A list item
