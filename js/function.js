21bb264cc09b3a00b0303d4535974a8ea0e02032
envose <envose.au@gmail.com>
2026-07-07 11:42:10 +0800

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-  const GAS_URL = 'https://script.google.com/macros/s/AKfycbxbX01HpXqo0n--6-cECc3HjOtdGz5qgYfscHGUXKfAYceyroH8cV3uoSHvEs_MlT-pJw/exec';
+  const GAS_URL = 'https://script.google.com/macros/s/AKfycbznnASs3W2yep0K-yKiXGDeZSsTJqQt1XgaDCTMsYNDM_Uqg97XGsx1H4b9SIxE2d8JAg/exec';
   const YOUR_CLIENT_ID = '207039464470-ds863khed849svfntdjertq4bpddpqva.apps.googleusercontent.com';
   const YOUR_REDIRECT_URI = 'https://envose.github.io';
 
