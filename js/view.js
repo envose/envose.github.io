5a776d5d8cc1d333008c0c3af47bf8eba77e2e67
envose <envose.au@gmail.com>
2026-07-07 10:29:29 +0800

    Update view.js

--- js/view.js
+++ js/view.js
@@ -153,7 +153,7 @@ function getFooterHtml() {
   html += '  <div class="container-fluid mx-1 my-1">';
   html += '    <div class="container navbar-brand col-12">';
   html += '    <div class="row">';
-  html += '      <div class="col text-center px-0"><button class="btn btn-light text-secondary" type="button"><i class="fa fa-history" style="font-size:32px;" onclick="return createTempView();"></i></button></div>';
+  html += '      <div class="col text-center px-0"><button class="btn btn-light text-primary" type="button"><i class="fa fa-history" style="font-size:32px;" onclick="return createHistView();"></i></button></div>';
   html += '      <div class="col text-center px-0"><button class="btn btn-light text-secondary  position-relative" type="button" onclick="return createTempView();"><i class="fa fa-question" style="font-size:32px;"></i>';
   html += '</button></div>';
   html += '      <div class="col text-center px-0"><button class="btn btn-light text-secondary  position-relative" type="button" onclick="return createTempView();"><i class="fa fa-question" style="font-size:32px;"></i>';
@@ -195,6 +195,12 @@ function createMainView() {
   html += '</div>';
   div.innerHTML = html;
 
+}
+
+function createHistView() {
+  var userinfo = getUserInfo();
+  var hist = userinfo.history;
+  alert(JSON.stringify(hist,0,2));
 }
 
 function createSorryView() {
